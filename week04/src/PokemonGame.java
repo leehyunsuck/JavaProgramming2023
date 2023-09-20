f204b31a77b0dad319d8bcdf14f5a53f56063ae6
leehyunsuck <leehyunsuck01@gmail.com>
2023-09-20 14:00:18 +0900

    Week04 Ver.17

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -3,6 +3,7 @@
 public class PokemonGame {
     public static void main(String[] args) {
         Pikachu pikachu = new Pikachu();
+//        pikachu.setFlyable(new NoFly());
         pikachu.performFly();
     }
 }

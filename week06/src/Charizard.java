3a228e6914d32fee467e11c71bbe3e60f010b7b2
leehyunsuck <leehyunsuck01@gmail.com>
2023-10-04 14:40:04 +0900

    Week06 Ver.28

---
+++ week06/src/Charizard.java
@@ -0,0 +1,28 @@
+
+
+public class Charizard extends Pokemon {
+    public Charizard(Flyable f) {
+        System.out.println("리이~자몽!");
+        this.level = 36;
+        this.attackRate = (int)(Math.random() * 11) + 74;
+        this.defenseRate = 78;
+        this.hp = 78;
+        this.name = "리자몽";
+        this.flyable = f;
+        this.skills = new String[]{"화염방사", "용의숨결", "할퀴기"};
+        this.specialAttackRate = new int[]{90, 60, 40};
+    }
+
+    public Charizard(int level, int hp, String name) {
+        super(level, hp, name);
+        System.out.println("자식 클래스(리자몽)의 매개변수 생성자 (생성자 오버로딩)");
+    }
+
+    @Override
+    public void attack(){
+        System.out.println(this.name + " 이(가) 플레어드라이브 공격 시전!");
+    }
+    public void type(){
+        System.out.println(this.name + " 은(는) 화염 속성!");
+    }
+}
---
+++ week06/src/Squirtle.java
@@ -0,0 +1,24 @@
+
+
+public class Squirtle extends Pokemon {
+    public Squirtle(Flyable f) {
+        System.out.println("꼬북꼬북!");
+        this.level = 1;
+        this.attackRate = (int)(Math.random() * 4) + 45;
+        this.defenseRate = 65;
+        this.hp = 44;
+        this.name = "꼬부기";
+        this.flyable = f;
+        this.skills = new String[]{"하이드로펌프", "물의파동", "물대포"};
+        this.specialAttackRate = new int[]{110, 60, 40};
+    }
+
+    public void type(){
+        System.out.println(this.name + " 은(는) 물 속성!");
+    }
+    
+    @Override
+    public void attack(){
+        System.out.println(this.name + " 이(가) 하이드로펌프 공격 시전!");
+    }
+}

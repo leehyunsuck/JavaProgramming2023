d6b4ad0e2ee67f8cd62cca62955c52a6d2b6af1f
leehyunsuck <leehyunsuck01@gmail.com>
2023-10-11 14:35:27 +0900

    Week07 Ver.35 6

--- week07/src/Pokemonster/PokemonGame.java
+++ week07/src/Pokemonster/PokemonGame.java
@@ -1,5 +1,6 @@
 package Pokemonster;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PokemonGame {
@@ -31,43 +32,48 @@ public class PokemonGame {
                 "   2] 꼬부기" +
                 "   3] 리자몽" +
                 " : ");
-        int pokemonPick = input.nextInt();
-        if (pokemonPick == 1) {
-            NoFly noFly = new NoFly();
-            player = new Pikachu(noFly);
-        } else if (pokemonPick == 2) {
-            NoFly noFly = new NoFly();
-            player = new Squirtle(noFly);
-        } else if (pokemonPick == 3) {
-            Wings wings = new Wings();
-            player = new Charizard(wings);
-        } else {
-            System.out.println("정상적인 선택이 아닙니다");
-        }
-
-        int menu, skillMenu;
-        while (true) {
-            System.out.print("\t[1]전투   [2]도망   [3]종료 : ");
-            menu = input.nextInt();
-
-            if (menu == 1) {
-                System.out.print("전투기술 : ");
-                int i = 1;
-                for (String skill : player.skills) {
-                    System.out.print(i + ") " + skill + " ");
-                    i++;
-                }
-
-                skillMenu = input.nextInt();
-                player.attack(enemy, skillMenu-1);
-
-                enemy.attack(player, (int)(Math.random() * 3));
-            } else if (menu == 2) {
-                
+        try {
+            int pokemonPick = input.nextInt();
+            if (pokemonPick == 1) {
+                NoFly noFly = new NoFly();
+                player = new Pikachu(noFly);
+            } else if (pokemonPick == 2) {
+                NoFly noFly = new NoFly();
+                player = new Squirtle(noFly);
+            } else if (pokemonPick == 3) {
+                Wings wings = new Wings();
+                player = new Charizard(wings);
             } else {
-                System.out.println("게임 종료");
-                break;
+                System.out.println("정상적인 선택이 아닙니다");
             }
+
+            int menu, skillMenu;
+            while (true) {
+                System.out.print("\t[1]전투   [2]도망   [3]종료 : ");
+                menu = input.nextInt();
+
+                if (menu == 1) {
+                    System.out.print("전투기술 : ");
+                    int i = 1;
+                    for (String skill : player.skills) {
+                        System.out.print(i + ") " + skill + " ");
+                        i++;
+                    }
+
+                    skillMenu = input.nextInt();
+                    player.attack(enemy, skillMenu-1);
+
+                    enemy.attack(player, (int)(Math.random() * 3));
+                } else if (menu == 2) {
+
+                } else {
+                    System.out.println("게임 종료");
+                    break;
+                }
+            }
+        } catch (InputMismatchException err) {
+            System.out.println("입력값은 숫자 만 가능합니다");
         }
+
     }
 }
